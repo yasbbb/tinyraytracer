4161d5f7fa0e7bc2481207a1000dd9e3ae3c83f1
Yash Bardhan <165434548+yashbbb009@users.noreply.github.com>
2024-06-15 04:08:04 -0500

    tweak main

--- src/Raycasting.cpp
+++ src/Raycasting.cpp
@@ -1,6 +1,3 @@
-// Raycasting.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
 #include <cmath>
 #include <SFML/Graphics.hpp>
